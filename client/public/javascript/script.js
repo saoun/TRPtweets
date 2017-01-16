cc3e3f7ed7f7ca34073b31c9b01791953749840b
sarah aoun <sarah@silk.co>
2017-01-16 18:34:54 -0500

    Add more js test

--- client/public/javascript/script.js
+++ client/public/javascript/script.js
@@ -1,60 +1,11 @@
 "use strict";
 (function(){
 
-//global variables
+//declaring variables
 var width = 500;
 var height = 500;
 var data
-
-
-//rendering data from server
-$.ajax({
-  url: '/get-data',
-  type: 'GET',
-})
-.done(function(response) {
-  data = response
-  console.log(response);
-  stringToNb(response);
-})
-.fail(function() {
-  console.log("error");
-});
-
-//d3 code starts here
-//
-var svg = d3.select('.chart')
-            .append('svg')
-            .attr('height', height)
-            .attr('width', width)
-            .append('g')
-            .attr('transform', 'translate(0,0)');
-
-//445 refers max tweets for one person
-var radiusScale = d3.scaleSqrt().domain([0, 445]).range([0,40])
-
-function makeCircles(d){
-  var circles = svg.selectAll('.target')
-                   .data(data)
-                   .enter().append('circle')
-                   .attr('class', 'target')
-                   .attr('r', function(d){
-                    return radiusScale(d.tweets)
-                   })
-}
-
-//turns back the string for number of tweets to an integer
-function stringToNb (data) {
-  data.forEach(function(d){
-    d.tweets = +d.tweets
-    makeCircles(d)
-  })
-};
-
-
-function applyForce(d){
-
-  //READ DOCS
+//READ DOCS
 var forceXSplit = d3.forceX(function(d){
   width * (d.gender === "m" ? 0.3 : 0.7)
 }).strength(0.2);
@@ -72,13 +23,75 @@ var forceCollide = d3.forceCollide(function(d){
 var simulation = d3.forceSimulation()
                    .force('x', forceXCombine)
                    .force('y', forceYCombine)
-                   .force('collide', forceCollide)
+                   .force('collide', forceCollide);
 
+var svg = d3.select('.chart')
+            .append('svg')
+            .attr('height', height)
+            .attr('width', width)
+            .append('g')
+            .attr('transform', 'translate(0,0)');
+
+//445 refers max tweets for one person
+var radiusScale = d3.scaleSqrt().domain([0, 445]).range([0,40])
+var tooltip = d3.select('body')
+                .append('div')
+                .attr('class', 'tooltip');
+                .text('')
+
+
+
+//rendering data from server
+$.ajax({
+  url: '/get-data',
+  type: 'GET',
+})
+.done(function(response) {
+  data = response
+  console.log(response);
+  stringToNb(response);
+})
+.fail(function() {
+  console.log("error");
+});
+
+//
+//
+function makeCircles(d){
+  var circles = svg.selectAll('.target')
+                   .data(data)
+                   .enter().append('circle')
+                   .attr('class', 'target')
+                   .attr('r', function(d){
+                    return radiusScale(d.tweets)
+                   });
+                   showTooltip
+                   simulation
 }
 
+//turns back the string for number of tweets to an integer
+function stringToNb (data) {
+  data.forEach(function(d){
+    d.tweets = +d.tweets
+    makeCircles(d)
+  })
+};
 
 
 
+var showTooltip = function(){
+  var mouseover = function(){
+    return tooltip.style('visibility','visible');
+  };
+
+  var mouseout = function() {
+    return tooltip.style('visibility', 'hidden')
+  };
+
+  var mousemove = function(){
+    return tooltip.style('top', (event.pageY-10)+'px').style('left',(d3.event.pageX+10)+'px')
+  };
+}
 
 
 
