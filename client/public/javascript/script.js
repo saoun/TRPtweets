767ceeb8550a5fd0037a24b664a43dfe20c89365
sarah aoun <sarah@silk.co>
2017-01-16 14:58:20 -0500

    add ajax

--- client/public/javascript/script.js
+++ client/public/javascript/script.js
@@ -1,4 +1,29 @@
+"use strict";
+(function(){
+
+$.ajax({
+  url: '/get-data',
+  type: 'GET',
+})
+.done(function(response) {
+  console.log(response);
+
+})
+.fail(function() {
+  console.log("error");
+})
+});
+
+
+
+
+
+
 d3.queue()
   .defer(d3.csv, "../../trumptweets.csv")
 
 
+
+
+
+})();
