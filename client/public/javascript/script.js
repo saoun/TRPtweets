2ef959166a8712537a12bf8686fcab707f6eb5a2
sarah aoun <sarah@silk.co>
2017-01-25 18:08:01 -0500

    Add notes

--- client/public/javascript/script.js
+++ client/public/javascript/script.js
@@ -180,9 +180,13 @@ function setupButtons(){
   d3.select('#toolbar')
     .selectAll('.button')
     .on('click', function(){
+      // Remove active class from all buttons
       d3.selectAll('.button').classed('active', false);
+      // Find the button just clicked
       var button = d3.select(this);
+      // Set it as the active button
       button.classed('active', true);
+      // Get the id of the button
       var buttonId = button.attr('id');
 
     })
