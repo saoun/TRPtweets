f6984061704a9084c8cdaf40bc24ceaced217ff4
sarah aoun <sarah@silk.co>
2017-01-25 18:02:01 -0500

    add button js

--- client/public/javascript/script.js
+++ client/public/javascript/script.js
@@ -176,6 +176,22 @@ function makeMagic(data){
   startForces(parsedData, circles)
 }
 
+function setupButtons(){
+  d3.select('#toolbar')
+    .selectAll('.button')
+    .on('click', function(){
+      d3.selectAll('.button').classed('active', false);
+      var button = d3.select(this);
+      button.classed('active', true);
+      var buttonId = button.attr('id');
+
+    })
+}
+
+setupButtons()
+
+
+
 
 })();
 
