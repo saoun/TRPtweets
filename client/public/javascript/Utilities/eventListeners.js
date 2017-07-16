c0e55e0395dac066710ebd509bb0b0a4d1a131c0
Jeff <jeffahking@gmail.com>
2017-07-16 19:34:00 -0400

    Add hamburger X menu

--- client/public/javascript/Utilities/eventListeners.js
+++ client/public/javascript/Utilities/eventListeners.js
@@ -1,3 +1,9 @@
+$(document).ready(function(){
+  $('#nav-icon1,#nav-icon2,#nav-icon3,#nav-icon4').click(function(){
+    $(this).toggleClass('open');
+  });
+});
+
 function buttonClicked(buttonId){
 
   window.scrollTo(0,0)
