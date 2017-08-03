089a34869d32345644bdaa7fabff9cc87c5f933f
sarah aoun <sarah@silk.co>
2017-08-03 12:54:31 -0400

    Edit modal

--- client/public/javascript/Utilities/modal.js
+++ client/public/javascript/Utilities/modal.js
@@ -1,10 +1,37 @@
-var elements = $('.modal-overlay, .modal');
+// var elements = $('.modal-overlay, .modal');
 
-$('#nav-icon2').click(function(){
-    elements.addClass('active');
-    console.log('hello')
-});
+// $('#nav-icon2').click(function(){
+//     elements.addClass('active');
+//     console.log('hello')
+// });
 
-$('.close-modal').click(function(){
-    elements.removeClass('active');
-});
+// $('.close-modal').click(function(){
+//     elements.removeClass('active');
+// });
+
+
+// Get the modal
+var modal = document.getElementById('myModal');
+
+// Get the button that opens the modal
+var btn = document.getElementById("nav-icon2");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal
+btn.onclick = function() {
+    modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
